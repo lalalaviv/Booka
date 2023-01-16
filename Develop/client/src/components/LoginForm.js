0601d4e191f29823b8d88aa6e3da262248c58221
lalalaviv <lalala.viv@hotmail.com>
2023-01-16 14:04:01 +0800

    updated forms so it is functioning with graphql

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -1,11 +1,12 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
 
 const LoginForm = () => {
+  const [login, {error} ] = useMutation(LOGIN_USER);
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,15 +27,19 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      //const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      //if (!response.ok) {
+       // throw new Error('something went wrong!');
+      //}
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      //const { token, user } = await response.json();
+     // console.log(user);
+     // Auth.login(token);
+     const { data } = await login({
+      variables: {...userFormData}
+  });
+  Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -84,6 +89,7 @@ const LoginForm = () => {
           variant='success'>
           Submit
         </Button>
+        {error && <div>Login failed</div>}
       </Form>
     </>
   );
--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/react-hooks';
+
 const SignupForm = () => {
+  const [addUser, { error }] = useMutation(ADD_USER);
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
@@ -21,22 +23,28 @@ const SignupForm = () => {
     event.preventDefault();
 
     // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    //const form = event.currentTarget;
+    //if (form.checkValidity() === false) {
+      //event.preventDefault();
+      //event.stopPropagation();
+    //}
 
     try {
-      const response = await createUser(userFormData);
+      //const response = await createUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      //if (!response.ok) {
+       // throw new Error('something went wrong!');
+      //}
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({
+        variables: {...userFormData}
+    });
+
+    Auth.login(data.addUser.token);
+
+      //const { token, user } = await response.json();
+      //console.log(user);
+      //Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -102,6 +110,7 @@ const SignupForm = () => {
           variant='success'>
           Submit
         </Button>
+        {error && <div>Sign up failed</div>}
       </Form>
     </>
   );
